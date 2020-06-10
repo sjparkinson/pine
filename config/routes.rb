f001377ef415fec04434d27041273697c1a0971a
Samuel Parkinson <sam.james.parkinson@gmail.com>
2020-06-10 12:51:06 +0100

    Add /healthz route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'index#index'
 
+  get 'healthz', to: 'index#healthz'
+
   get 'login', to: 'sessions#new', as: 'login'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy', as: 'logout'
